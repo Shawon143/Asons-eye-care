38975ca6a52d65b0f6cdbc9495188202c6e1354f
Shawon143 <sishawon07@gmail.com>
2021-10-19 21:06:51 +0600

    first

--- src/components/servicedetails/ServiceDetails.js
+++ src/components/servicedetails/ServiceDetails.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import "./singleservice.css";
 
 const ServiceDetails = () => {
   const { serviceName } = useParams();
 
   return (
-    <div>
+    <div className="single_service">
       <h2>This is {serviceName} </h2>
     </div>
   );
